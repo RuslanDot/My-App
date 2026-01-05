ff5deaf0a0a8b7fea18e11cd41f7080415854dbe
Ruslan Vasyliev <0675560258rusl@gmail.com>
2026-01-05 22:31:08 +0000

    Add Lesson2 homework

---
+++ src/homework/Lesson2.java
@@ -0,0 +1,39 @@
+package homework;
+
+public class Lesson2 {
+
+    public static void main(String[] args) {
+
+     int OrderNo;
+     String  Client;
+     String  Product;
+     double price;
+     String  Address;
+
+      OrderNo = 1;
+      Client = "Alice";
+      Product = "smartphone";
+      price = 305.99;
+      Address = "Moon Street, 10";
+
+System.out.println( "Order No " + OrderNo + " Client: " + Client +".");
+System.out.println( "Product: " + Product + ",");
+System.out.println( "price EUR " + price + ".");
+System.out.println( "Address: " + Address + ".");
+
+
+        OrderNo = 2;
+        Client = "Tom";
+        Product = "laptop";
+        price = 570.95;
+        Address = "Terra Street, 17";
+
+        System.out.println( "Order No " + OrderNo + " Client: " + Client +".");
+        System.out.println( "Product: " + Product + ",");
+        System.out.println( "price EUR " + price + ".");
+        System.out.println( "Address: " + Address + ".");
+
+
+
+    }
+}
